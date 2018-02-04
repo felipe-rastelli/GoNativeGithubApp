7696888533a2e54d6e097e87fc78764d190db674
Rastelli <felipe-rastelli@hotmail.com>
2018-02-04 20:07:43 -0300

    class App modified, class RepoList added

--- App.js
+++ App.js
@@ -1,51 +1,76 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  Container,
+  Content,
+  Header,
+  Body,
+  Title,
+  Form,
+  Item,
+  Icon,
+  Input,
+  Spinner,
+  Button,
+  Text
+} from 'native-base';
+
+import RepoList from './RepoList';
 
 export default class App extends Component<{}> {
+
+  state = {
+    search: '',
+    loading: false,
+    repositories: []
+  };
+
+  fetchRepositories = async () => {
+    if(this.state.search.length > 0){
+      this.setState({ loading: true });
+
+      const response = await fetch(`https://api.github.com/search/repositories?q=${this.state.search}`);
+      const repositories = await response.json();
+
+      this.setState({
+        repositories: repositories.items,
+        loading: false
+      })
+    }
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title>Github Explorer</Title>
+          </Body>
+        </Header>
+        <Content padder>
+          <Form>
+            <Item last>
+              <Icon active name="search"/>
+              <Input 
+                placeholder="Buscar palavra chave" 
+                value={this.state.search}
+                onChangeText={text => this.setState({ search: text })}
+              />
+            </Item>
+          </Form>
+          <Button 
+            block 
+            style={{ marginTop: 10 }}
+            onPress={this.fetchRepositories}
+          >
+            <Text>Buscar</Text>
+          </Button>
+
+          { this.state.loading 
+            ? <Spinner color="#999"/>
+            : <RepoList repositories={this.state.repositories}/> }
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
